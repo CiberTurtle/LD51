a51f3e15712a63fa13c0bc46cb90963490162cd6
CiberTurtle <rylydou@gmail.com>
2022-10-01 17:35:06 -0500

    Sick springs

--- Assets/Launcher/Launcher.gd
+++ Assets/Launcher/Launcher.gd
@@ -5,12 +5,14 @@ const Player = preload("res://Assets/Player/Player.gd")
 export var hSpeed = 600
 export var vSpeed = -400
 
-func _ready():
-	$Flip.scale.x = sign(hSpeed);
-
 func _on_Area2D_body_entered(body):
 	var player = body as Player
+	# Skip non players
 	if not player: return
 
+	# Apply velocity
 	player.hExtraSpeed = hSpeed
 	player.vSpeed = vSpeed
+	player.coyoteTimer = -1
+
+	$AnimationPlayer.play('Spring Bounce')
