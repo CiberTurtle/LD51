3d3ab793d3db15e7aa6425a90b4caf6aa6fcc40d
CiberTurtle <rylydou@gmail.com>
2022-10-02 16:34:47 -0500

    Stuffs

--- Assets/Boost/Boost.gd
+++ Assets/Boost/Boost.gd
@@ -5,6 +5,19 @@ const Player = preload("res://Assets/Player/Player.gd")
 export var strength = 400
 export var snapDirection = true
 
+export var idleSpeed = 1.0
+export var boostSpeed = 8.0
+export var speedDec = 2.0
+
+var speed = 0;
+
+func _ready():
+	$AnimationPlayer.play('Idle')
+
+func _process(delta):
+	$Art/Plus.rotation += speed * TAU * delta
+	speed = move_toward(speed, idleSpeed, speedDec * delta);
+
 func _on_Boost_body_entered(body):
 	var player = body as Player
 	# Skip non players
@@ -15,5 +28,7 @@ func _on_Boost_body_entered(body):
 	player.vSpeed = sign(player.vSpeed) * strength
 	player.coyoteTimer = -1
 
-	# $AnimationPlayer.play('Spring Bounce')
-	# $AudioStreamPlayer2D.play()
+	speed = boostSpeed
+
+	$AudioStreamPlayer2D.play()
+	$CPUParticles2D.emitting = true;
